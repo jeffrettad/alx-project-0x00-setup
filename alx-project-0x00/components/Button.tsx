58b9877ab32c8f97c69ca502dc72ddc3906539e3
Adesoye Adedokun <57882971+jeffrettad@users.noreply.github.com>
2025-06-01 00:44:31 -0700

    Update Button.tsx

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -3,7 +3,7 @@ Import React from "react";
 Import { ButtonProps) from "@/interfaces";
 const Button: React.FC<ButtonProps> = ({title, styles}) =>{
 return (
-<button className-["bg-blue-600 px-3 w-auto h-[27px] text-white ${styles}"}>
+<button className-["bg-blue-600 px-3 w-auto h-[27px] text-white ${styles} "rounded-sm", "rounded-lg", "rounded-full""}>
   {title}
 </button>
 );
