d176bce042915ef5acd7fd0047c242aab716405e
Adesoye Adedokun <57882971+jeffrettad@users.noreply.github.com>
2025-06-01 00:38:16 -0700

    Update Button.tsx

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,1 +1,12 @@
 
+Import React from "react";
+Import { ButtonProps) from "@/interfaces";
+const Button: React.FC<ButtonProps> = ({title, styles}) =>{
+return (
+<button className-["bg-blue-600 px-3 w-auto h-[27px] text-white ${styles}"}>
+  {title}
+</button>
+);
+}
+export default Button;
+
